2ba5b9eda2840495100935c2a1417dedbf5da441
Satori32 <Huga8989+githubf@gmail.com>
2019-08-17 18:52:56 +0900

    プロジェクト ファイルを追加します。

---
+++ 日本語プリプロセッサ？？/Header.h
@@ -0,0 +1,60 @@
+#pragma once
+
+//license for others by GPLv3.
+
+#include <string>
+#include <cstdint>
+#include <vector>
+
+#define ŠJn“_ main
+
+
+#define ‚±‚±‚©‚ç {
+#define ‚±‚±‚Ü‚Å }
+
+#define ®” int
+#define À” double
+#define •¶š—ñ std::string
+#define ”z—ñ std::vector
+#define ƒ <
+#define „ >
+
+#define ’è const
+#define Ã static
+
+#define ‚P 1
+#define ‚Q 2
+#define ‚R 3
+#define ‚S 4
+#define ‚T 5
+#define ‚U 6
+#define ‚V 7
+#define ‚W 8
+#define ‚X 9
+#define ‚O 0
+
+#define { +
+#define | -
+#define – *
+#define ^ /
+
+#define G  ; @
+#define  =
+
+#define •Ô‹p return 
+
+#define Œ^’è‹` typedef
+#define label
+#define ƒ‰ƒxƒ‹
+#define F :
+
+#define ŒJ‚è•Ô‚µ‚P(B) while(B)
+#define ŒJ‚è•Ô‚µ‚R(F,Lim,Plus) for(std::intmax_t X__ = F;X__<Lim;Plus)
+#define ‚â‚é do
+
+#define ‚à‚µ if(
+#define ‚È‚ç )
+#define ˆá‚¤‚È‚ç else
+
+
+
---
+++ 日本語プリプロセッサ？？/Source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "japanese.hpp"
+
+®” “Ã = 42;
+
+®” ŠJn“_ () ‚±‚±‚©‚ç
+	•Ô‹p ‚O;
+‚±‚±‚Ü‚Å
+
